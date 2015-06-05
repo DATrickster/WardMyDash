316fad8e435c5dbfab801a395cdb1b9a77cca242
DATrickster <1docter@gmail.com>
2015-06-05 15:09:09 -0600

    Update Sigils.user.js

--- Sigils.user.js
+++ Sigils.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Ward My Dash
-// @namespace    https://5eabe5388d40d6d55d69670321dac657a25c3ce7.googledrive.com/host/0B4jyzgJRuqLSU0N3bHBGelA3LWM/Sigils/
+// @namespace    https://github.com/DATrickster/WardMyDash
 // @version      0.37
 // @description  Adds warding sigils to your dashboard.
 // @author       The Trickster
 // @include      *://www.tumblr.com/dashboard
-// @updateURL    https://5eabe5388d40d6d55d69670321dac657a25c3ce7.googledrive.com/host/0B4jyzgJRuqLSU0N3bHBGelA3LWM/Sigils/Sigils.user.js
-// @downloadURL  https://5eabe5388d40d6d55d69670321dac657a25c3ce7.googledrive.com/host/0B4jyzgJRuqLSU0N3bHBGelA3LWM/Sigils/Sigils.user.js
+// @updateURL    https://github.com/DATrickster/WardMyDash/blob/master/Sigils.user.js
+// @downloadURL  https://github.com/DATrickster/WardMyDash/blob/master/Sigils.user.js
 // @grant        none
 // ==/UserScript==
 
