190bbce6a68a9f2b7b8d5a4c168f8b1e3f71508c
DATrickster <1docter@gmail.com>
2018-05-08 16:29:30 -0600

    Update Sigils.user.js

--- Sigils.user.js
+++ Sigils.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ward My Dash
 // @namespace    https://github.com/DATrickster/WardMyDash
-// @version      0.45
+// @version      0.5
 // @description  Adds warding sigils to your dashboard.
 // @author       Tricky the Squirrel
 // @include      *://www.tumblr.com/dashboard
@@ -11,10 +11,10 @@
 // ==/UserScript==
 
 
-
-var iDiv = document.createElement('style');
-iDiv.innerHTML += '.tx-scroll{position:fixed; bottom:63px; !important;}';
-document.getElementsByTagName('head')[0].appendChild(iDiv);
+var COMMAND = document.createElement('div');
+COMMAND.id = 'OBEY';
+document.getElementsByClassName('scroller')[0].appendChild(COMMAND);
+document.getElementById('OBEY').parentElement.parentElement.style="position:fixed; bottom:63px;";
 var sDiv = document.createElement('div');
 sDiv.id = 'sigils';
 sDiv.style = 'position: fixed; z-index: 9999;';
