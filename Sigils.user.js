c4dbc79baccbc79a0ded28df18ae8006a64732e6
DATrickster <1docter@gmail.com>
2018-05-08 14:50:42 -0600

    Update Sigils.user.js

--- Sigils.user.js
+++ Sigils.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ward My Dash
 // @namespace    https://github.com/DATrickster/WardMyDash
-// @version      0.43
+// @version      0.44
 // @description  Adds warding sigils to your dashboard.
 // @author       Tricky the Squirrel
 // @include      *://www.tumblr.com/dashboard
